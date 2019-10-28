e989b026b06d1fd05268ec432a4fb3bd62db956a
Viet Tran <tranhuyviet@gmail.com>
2019-10-28 13:36:28 +0200

    create cart-item component and connected with card-dropdown with cart-item reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import "./cart-dropdown.styles.scss";
 
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = () => (
-  <div className="cart-dropdown">
-    <div className="cart-items" />
-    <CustomButton>go to checkout</CustomButton>
-  </div>
-);
+import { connect } from "react-redux";
 
-export default CartDropdown;
+const CartDropdown = ({ cartItems }) => {
+  console.log(cartItems);
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.map(item => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
+      <CustomButton>go to checkout</CustomButton>
+    </div>
+  );
+};
+
+const mapStateToProps = ({ cart: { cartItems } }) => {
+  return {
+    cartItems
+  };
+};
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import { CartActionTypes } from "./cart.types";
+import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -13,9 +14,28 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         hidden: !state.hidden
       };
     case CartActionTypes.ADD_ITEM:
+      // let item = action.payload;
+      // let cartTemp = [...state.cartItems];
+      // let itemTemp = cartTemp.find(cart => cart.id === item.id);
+
+      // if (!itemTemp) {
+      //   //not exist
+      //   let count = 1;
+      //   let totalPrice = item.price;
+
+      //   let cartItem = { ...item, count, totalPrice };
+      //   cartTemp = [...cartTemp, cartItem];
+      // } else {
+      //   //exist already
+      //   itemTemp.count++;
+      //   itemTemp.totalPrice = itemTemp.count * itemTemp.price;
+      //   itemTemp.totalPrice = parseFloat(itemTemp.totalPrice.toFixed(2));
+      // }
+
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload]
+        //cartItems: cartTemp
+        cartItems: addItemToCart(state.cartItems, action.payload)
       };
     default:
       return state;
