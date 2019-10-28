bab904535beee262bd55dbf1a2dd795bd039213a
Viet Tran <tranhuyviet@gmail.com>
2019-10-28 10:05:54 +0200

    implement add-cart button and handle cart reducer and cart action

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -3,3 +3,11 @@ import { CartActionTypes } from "./cart.types";
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
+
+export const addItem = item => {
+  //console.log("action item", item);
+  return {
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+  };
+};
